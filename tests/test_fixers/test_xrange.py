38316e95ea51a5184f5ef719b159bd9bcd4f52b7
Timo Stüber <omit66@gmail.com>
2018-07-17 11:14:23 +0200

    added some more test for range

--- tests/test_fixers/test_xrange.py
+++ tests/test_fixers/test_xrange.py
@@ -36,11 +36,6 @@ class Test_xrange(FixerTestCase):
         a = """import six.moves\nx = six.moves.xrange(0, 10, 2)"""
         self.check(b, a)
 
-    def test_range(self):
-        a = """x = range(10, 3, 9)"""
-        b = """import past.builtins\nx = past.builtins.range(10, 3, 9)"""
-        self.check(a, b)
-
     def test_xrange_in_for(self):
         a = """for i in xrange(10):\n    j=i"""
         b = """import six.moves\nfor i in six.moves.xrange(10):\n    j=i"""
@@ -50,16 +45,38 @@ class Test_xrange(FixerTestCase):
         b = """import six.moves\n[i for i in six.moves.xrange(10)]"""
         self.check(a, b)
 
-    def test_range_in_for(self):
-        a = "for i in range(10): pass"
-        b = "import past.builtins\nfor i in past.builtins.range(10): pass"
-        self.check(a, b)
-        c = "[i for i in range(10)]"
-        d = "import past.builtins\n[i for i in past.builtins.range(10)]"
-        self.check(c, d)
-
     def test_in_consuming_context(self):
         for call in fixer_util.consuming_calls:
             a = "a = %s(xrange(10))" % call
             b = "import six.moves\na = %s(six.moves.xrange(10))" % call
             self.check(a, b)
+            a = "a = %s(range(10))" % call
+            b = "import past.builtins\na = %s(past.builtins.range(10))" % call
+            self.check(a, b)
+
+    def test_range_in_for(self):
+        a = "for i in range(9): pass"
+        b = "import past.builtins\nfor i in past.builtins.range(9): pass"
+        self.check(a, b)
+        c = "[i for i in range(9)]"
+        d = "import past.builtins\n[i for i in past.builtins.range(9)]"
+        self.check(c, d)
+
+    def test_range(self):
+        a = """x = range(10, 3, 9)"""
+        b = """import past.builtins\nx = past.builtins.range(10, 3, 9)"""
+        self.check(a, b)
+
+    def test_range_prefix_preservation(self):
+        b = """x =    range(  10  )"""
+        a = """import past.builtins\nx =    past.builtins.range(  10  )"""
+        self.check(b, a)
+
+        b = """x = range(  1  ,  10   )"""
+        a = """import past.builtins\nx = past.builtins.range(  1  ,  10   )"""
+        self.check(b, a)
+
+        b = """x = range(  0  ,  10 ,  2 )"""
+        a = """import past.builtins\n"""\
+            """x = past.builtins.range(  0  ,  10 ,  2 )"""
+        self.check(b, a)
