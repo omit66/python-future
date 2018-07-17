88b24ad03fe191b62fef7f899f713938fe02042b
Timo Stüber <omit66@gmail.com>
2018-07-17 10:52:27 +0200

    expected behaviour changed

--- tests/test_fixers/test_xrange.py
+++ tests/test_fixers/test_xrange.py
@@ -6,55 +6,60 @@ from fixertestcase import FixerTestCase
 
 
 class Test_xrange(FixerTestCase):
-    fixer = "xrange_with_six"
+    fixer = "xrange"
 
     def test_prefix_preservation(self):
         b = """x =    xrange(  10  )"""
-        a = """from six.moves import xrange\nx =    xrange(  10  )"""
+        a = """import six.moves\nx =    six.moves.xrange(  10  )"""
         self.check(b, a)
 
         b = """x = xrange(  1  ,  10   )"""
-        a = """from six.moves import xrange\nx = xrange(  1  ,  10   )"""
+        a = """import six.moves\nx = import six.moves.xrange(  1  ,  10   )"""
         self.check(b, a)
 
         b = """x = xrange(  0  ,  10 ,  2 )"""
-        a = """from six.moves import xrange\nx = xrange(  0  ,  10 ,  2 )"""
+        a = """import six.moves\nx = six.moves.xrange(  0  ,  10 ,  2 )"""
         self.check(b, a)
 
     def test_single_arg(self):
         b = """x = xrange(10)"""
-        a = """from six.moves import xrange\nx = xrange(10)"""
+        a = """import six.moves\nx = six.moves.xrange(10)"""
         self.check(b, a)
 
     def test_two_args(self):
         b = """x = xrange(1, 10)"""
-        a = """from six.moves import xrange\nx = xrange(1, 10)"""
+        a = """six.moves\nx = six.moves.xrange(1, 10)"""
         self.check(b, a)
 
     def test_three_args(self):
         b = """x = xrange(0, 10, 2)"""
-        a = """from six.moves import xrange\nx = xrange(0, 10, 2)"""
+        a = """import six.moves\nx = six.moves.xrange(0, 10, 2)"""
         self.check(b, a)
 
-    def test_range_unchange(self):
+    def test_range(self):
         a = """x = range(10, 3, 9)"""
-        self.unchanged(a)
+        b = """import past.builtins\nx = past.builtins.range(10, 3, 9)"""
+        self.check(a, b)
 
     def test_xrange_in_for(self):
         b = """for i in xrange(10):\n    j=i"""
-        a = """from six.moves import xrange\nfor i in xrange(10):\n    j=i"""
+        a = """import six.moves\nfor i in six.moves.xrange(10):\n    j=i"""
         self.check(b, a)
 
         b = """[i for i in xrange(10)]"""
-        a = """from six.moves import xrange\n[i for i in xrange(10)]"""
+        a = """import six.moves\n[i for i in six.moves.xrange(10)]"""
         self.check(b, a)
 
-    def test_range_in_for_unchanged(self):
-        self.unchanged("for i in range(10): pass")
-        self.unchanged("[i for i in range(10)]")
+    def test_range_in_for(self):
+        a = "for i in range(10): pass"
+        b = "import past.builtins\nfor i in past.builtins.range(10): pass"
+        self.check(a, b)
+        c = "[i for i in range(10)]"
+        d = "import past.builtins.range\n[i for i in past.builtins.range(10)]"
+        self.check(c, d)
 
     def test_in_consuming_context(self):
         for call in fixer_util.consuming_calls:
             a = "a = %s(xrange(10))" % call
-            b = "from six.moves import xrange\na = %s(xrange(10))" % call
+            b = "import six.moves\na = %s(six.moves.xrange(10))" % call
             self.check(a, b)
