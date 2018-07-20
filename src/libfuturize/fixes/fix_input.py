fcf436e62ea56830082e0014550e79ed26b23633
Timo Stüber <omit66@gmail.com>
2018-07-20 13:13:02 +0200

    ship input

---
+++ src/libfuturize/fixes/fix_input.py
@@ -0,0 +1,59 @@
+# This is a derived work of Lib/lib2to3/fixes/fix_input.py and
+# Lib/lib2to3/fixes/fix_raw_input.py. Those files are under the
+# copyright of the Python Software Foundation and licensed under the
+# Python Software Foundation License 2.
+#
+# Copyright notice:
+#
+#     Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+#     2011, 2012, 2013, 2014 Python Software Foundation. All rights reserved.
+# This work has been extended by CONTACT Software GmbH
+from __future__ import absolute_import
+
+from lib2to3 import patcomp, fixer_base
+from lib2to3.fixer_util import Call, Name
+from libfuturize.fixer_util import touch_import_top
+
+
+class FixInput(fixer_base.ConditionalFix):
+
+    BM_compatible = True
+    order = 'pre'
+    skip_on = 'six.moves.input'
+
+    PATTERN = """
+              power< (name='input' | name='raw_input')
+                trailer< '(' [any] ')' > any* >
+              """
+
+    def transform(self, node, results):
+        if self.should_skip(node):
+            return
+
+        touch_import_top(None, u'six.moves', node)
+        name = results['name']
+        old_name = name.value
+        name.replace(Name('six.moves.input', prefix=name.prefix))
+        if old_name == 'input':
+            # if the node is wrapped in eval it not necessary to wrap again
+            if not self.in_special_context(node):
+                new_node = node.clone()
+                new_node.prefix = ''
+                return Call(Name('eval'), [new_node], prefix=node.prefix)
+
+    P1 = "parent=power< func=NAME trailer< '(' node=any ')' > any* >"
+    p1 = patcomp.compile_pattern(P1)
+
+    def in_special_context(self, node):
+        # it is not wrapped
+        if node.parent is None:
+            return False
+        results = {}
+        if (node.parent.parent is not None and
+           self.p1.match(node.parent.parent, results) and
+           results["node"] is node):
+
+            # eval(input) -> eval(six.moves.input)
+            if results['func'].value == 'eval':
+                return True
+        return False
---
+++ tests/test_fixers/test_input.py
@@ -0,0 +1,92 @@
+
+from fixertestcase import FixerTestCase
+
+
+class Test_input(FixerTestCase):
+    fixer = "input"
+
+    def test_prefix_preservation(self):
+        b = """x =    raw_input(   )"""
+        a = """import six.moves\nx =    six.moves.input(   )"""
+        self.check(b, a)
+
+        b = """x = raw_input(   ''   )"""
+        a = """import six.moves\nx = six.moves.input(   ''   )"""
+        self.check(b, a)
+
+        b = """x =   input(   )"""
+        a = """import six.moves\nx =   eval(six.moves.input(   ))"""
+        self.check(b, a)
+
+        b = """x = input(   ''   )"""
+        a = """import six.moves\nx = eval(six.moves.input(   ''   ))"""
+        self.check(b, a)
+
+    def test_1(self):
+        b = """x = raw_input()"""
+        a = """import six.moves\nx = six.moves.input()"""
+        self.check(b, a)
+
+        b = """x = input()"""
+        a = """import six.moves\nx = eval(six.moves.input())"""
+        self.check(b, a)
+
+    def test_2(self):
+        b = """x = raw_input('')"""
+        a = """import six.moves\nx = six.moves.input('')"""
+        self.check(b, a)
+
+        b = """x = input('')"""
+        a = """import six.moves\nx = eval(six.moves.input(''))"""
+        self.check(b, a)
+
+    def test_3(self):
+        b = """x = raw_input('prompt')"""
+        a = """import six.moves\nx = six.moves.input('prompt')"""
+        self.check(b, a)
+
+        b = """x = input('prompt')"""
+        a = """import six.moves\nx = eval(six.moves.input('prompt'))"""
+        self.check(b, a)
+
+    def test_4(self):
+        b = """x = raw_input(foo(a) + 6)"""
+        a = """import six.moves\nx = six.moves.input(foo(a) + 6)"""
+        self.check(b, a)
+
+        b = """x = input(foo(5) + 9)"""
+        a = """import six.moves\nx = eval(six.moves.input(foo(5) + 9))"""
+        self.check(b, a)
+
+    def test_5(self):
+        b = """x = raw_input(invite).split()"""
+        a = """import six.moves\nx = six.moves.input(invite).split()"""
+        self.check(b, a)
+
+    def test_6(self):
+        b = """x = raw_input(invite) . split ()"""
+        a = """import six.moves\nx = six.moves.input(invite) . split ()"""
+        self.check(b, a)
+
+    def test_8(self):
+        b = "x = int(raw_input())"
+        a = "import six.moves\nx = int(six.moves.input())"
+        self.check(b, a)
+
+    def test_trailing_comment(self):
+        b = """x = input()  #  foo"""
+        a = """import six.moves\nx = eval(six.moves.input())  #  foo"""
+        self.check(b, a)
+
+    def test_wrapped_input(self):
+        b = """x = eval(input())"""
+        a = """import six.moves\nx = eval(six.moves.input())"""
+        self.check(b, a)
+
+        b = """x = eval(input(''))"""
+        a = """import six.moves\nx = eval(six.moves.input(''))"""
+        self.check(b, a)
+
+        b = """x = eval(input(foo(5) + 9))"""
+        a = """import six.moves\nx = eval(six.moves.input(foo(5) + 9))"""
+        self.check(b, a)
