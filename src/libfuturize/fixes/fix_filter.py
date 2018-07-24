eed3d1e01dc1ac3fcacb495adb60e9215cbba5cc
Timo Stüber <omit66@gmail.com>
2018-07-24 16:52:02 +0200

    create a fixer for filter

---
+++ src/libfuturize/fixes/fix_filter.py
@@ -0,0 +1,88 @@
+# Copyright 2007 Google, Inc. All Rights Reserved.
+# Licensed to PSF under a Contributor Agreement.
+# Extended by CONTACT Software GmbH
+
+"""Fixer that changes filter(F, X) into list(six.moves.filter(F, X)).
+
+We avoid the transformation if the filter() call is directly contained
+in iter(<>), list(<>), tuple(<>), sorted(<>), ...join(<>), or
+for V in <>:.
+
+NOTE: This is still not correct if the original code was depending on
+filter(F, X) to return a string if X is a string and a tuple if X is a
+tuple.  That would require type inference, which we don't do.  Let
+Python 2.6 figure it out.
+"""
+
+# Local imports
+from lib2to3 import fixer_base, pytree
+from lib2to3.fixer_util import Name, Call, ListComp, in_special_context
+
+from libfuturize.fixer_util import touch_import_top
+
+
+class FixFilter(fixer_base.ConditionalFix):
+    BM_compatible = True
+
+    PATTERN = """
+    filter_lambda=power<
+        func='filter'
+        trailer<
+            '('
+            arglist<
+                lambdef< 'lambda'
+                         (fp=NAME | vfpdef< '(' fp=NAME ')'> ) ':' xp=any
+                >
+                ','
+                it=any
+            >
+            ')'
+        >
+    >
+    |
+    power<
+        func='filter'
+        args=trailer< '(' arglist< none='None' ',' seq=any > ')' >
+        rest=any*
+    >
+    |
+    power<
+        func='filter'
+        args=trailer< '(' [any] ')' >
+        rest=any*
+    >
+    """
+
+    skip_on = "six.moves.filter"
+
+    def transform(self, node, results):
+        if self.should_skip(node):
+            return
+
+        if "filter_lambda" in results:
+            new = ListComp(results.get("fp").clone(),
+                           results.get("fp").clone(),
+                           results.get("it").clone(),
+                           results.get("xp").clone())
+
+        elif "none" in results:
+            new = ListComp(Name(u"_f"),
+                           Name(u"_f"),
+                           results["seq"].clone(),
+                           Name(u"_f"))
+
+        else:
+            if in_special_context(node):
+                return None
+            func = results['func']
+            touch_import_top(None, u'six.moves', node)
+            new = pytree.Node(self.syms.power,
+                              [Name(u"six.moves.filter", func.prefix),
+                               results['args'].clone()])
+            new.prefix = u""
+            new = Call(Name(u"list"), [new])
+            if "rest" in results:
+                for child in results["rest"]:
+                    new.append_child(child)
+        new.prefix = node.prefix
+        return new
---
+++ tests/test_fixers/test_filter.py
@@ -0,0 +1,90 @@
+from fixertestcase import FixerTestCase
+
+
+class Test_filter(FixerTestCase):
+    fixer = "filter"
+
+    def test_prefix_preservation(self):
+        b = """x =   filter(    foo,     'abc'   )"""
+        a = """import six.moves\n"""\
+            """x =   list(six.moves.filter(    foo,     'abc'   ))"""
+        self.check(b, a)
+
+        b = """x =   filter(  None , 'abc'  )"""
+        a = """x =   [_f for _f in 'abc' if _f]"""
+        self.check(b, a)
+
+    def test_filter_basic(self):
+        b = """x = filter(None, 'abc')"""
+        a = """x = [_f for _f in 'abc' if _f]"""
+        self.check(b, a)
+
+        b = """x = len(filter(f, 'abc'))"""
+        a = """import six.moves\nx = len(list(six.moves.filter(f, 'abc')))"""
+        self.check(b, a)
+
+        b = """x = filter(lambda x: x%2 == 0, range(10))"""
+        a = """x = [x for x in range(10) if x%2 == 0]"""
+        self.check(b, a)
+
+        # Note the parens around x
+        b = """x = filter(lambda (x): x%2 == 0, range(10))"""
+        a = """x = [x for x in range(10) if x%2 == 0]"""
+        self.check(b, a)
+
+        # XXX This (rare) case is not supported
+        b = """x = filter(f, 'abc')[0]"""
+        a = """import six.moves\nx = list(six.moves.filter(f, 'abc'))[0]"""
+        self.check(b, a)
+
+    def test_filter_nochange(self):
+        a = """b.join(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """(a + foo(5)).join(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """iter(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """list(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """list(filter(f, 'abc'))[0]"""
+        self.unchanged(a)
+        a = """set(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """set(filter(f, 'abc')).pop()"""
+        self.unchanged(a)
+        a = """tuple(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """any(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """all(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """sum(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """sorted(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """sorted(filter(f, 'abc'), key=blah)"""
+        self.unchanged(a)
+        a = """sorted(filter(f, 'abc'), key=blah)[0]"""
+        self.unchanged(a)
+        a = """enumerate(filter(f, 'abc'))"""
+        self.unchanged(a)
+        a = """enumerate(filter(f, 'abc'), start=1)"""
+        self.unchanged(a)
+        a = """for i in filter(f, 'abc'): pass"""
+        self.unchanged(a)
+        a = """[x for x in filter(f, 'abc')]"""
+        self.unchanged(a)
+        a = """(x for x in filter(f, 'abc'))"""
+        self.unchanged(a)
+
+    def test_future_builtins(self):
+        a = "from six.moves import filter, spam, filter; filter(f, 'ham')"
+        self.unchanged(a)
+
+        b = """from six.moves import spam; x = filter(f, 'abc')"""
+        a = """import six.moves\nfrom six.moves import spam;"""\
+            """ x = list(six.moves.filter(f, 'abc'))"""
+        self.check(b, a)
+
+        a = "from six.moves import *; filter(f, 'ham')"
+        self.unchanged(a)
