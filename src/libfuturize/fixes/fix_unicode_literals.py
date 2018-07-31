964da31b731d6ef87d39af1c9ab1bab5fc5ba318
Timo Stüber <omit66@gmail.com>
2018-07-31 13:21:19 +0200

    add fixer is not needed anymore

---
+++ src/libfuturize/fixes/fix_unicode_literals.py
@@ -0,0 +1,30 @@
+import re
+from lib2to3 import fixer_base
+from lib2to3.fixer_util import Name, Attr, Leaf
+from libfuturize.fixer_util import touch_import_top
+from lib2to3.pytree import Node
+from lib2to3.pgen2 import token
+
+_literal_re = re.compile(u"[uU][rR]?[\\'\\\"]")
+
+
+class FixUnicodeLiterals(fixer_base.BaseFix):
+    BM_compatible = True
+    PATTERN = """STRING"""
+
+    def transform(self, node, results):
+        if _literal_re.match(node.value):
+            touch_import_top(None, u'six', node)
+            new = Name(node.value[1:], prefix=u'')
+            new = self.createNewNode(new)
+            new.prefix = node.prefix
+            node.replace(new)
+
+    def createNewNode(self, node):
+        lpar = Leaf(token.LPAR, u'(')
+        rpar = Leaf(token.RPAR, u')')
+        new = Node(self.syms.power,
+                   Attr(Name(u"six"), Name(u"u")) +
+                   [Node(self.syms.trailer, [lpar, node, rpar])]
+                   )
+        return new
---
+++ tests/test_fixers/test_unicode_literals.py
@@ -0,0 +1,33 @@
+from fixertestcase import FixerTestCase
+
+
+class Test_unicode_literals(FixerTestCase):
+    fixer = "unicode_literals"
+
+    def test_unicode_literal_1(self):
+        b = '''s = u"x"'''
+        a = '''import six\ns = six.u("x")'''
+        self.check(b, a)
+
+    def test_unicode_literal_2(self):
+        b = """s = ur'x'"""
+        a = """import six\ns = six.u(r'x')"""
+        self.check(b, a)
+
+    def test_unicode_literal_3(self):
+        b = """s = UR'''x''' """
+        a = """import six\ns = six.u(R'''x''') """
+        self.check(b, a)
+
+    def test_u(self):
+        b = """a = u'test' in b"""
+        a = """import six\na = six.u('test') in b"""
+        self.check(b, a)
+
+        b = """s = u'test'"""
+        a = """import six\ns = six.u('test')"""
+        self.check(b, a)
+
+    def test_unchanged(self):
+        s = """r'test'"""
+        self.unchanged(s)
