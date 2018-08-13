cbb54f1af949d89007820ac78edd604f2a16c4fc
Timo Stüber <omit66@gmail.com>
2018-08-13 15:25:02 +0200

    added fixer for zip

---
+++ src/libfuturize/fixes/fix_zip.py
@@ -0,0 +1,42 @@
+"""
+Fixer that changes zip(seq0, seq1, ...) into
+list(six.moves.zip(seq0, seq1, ...) unless there exists a
+'from six.moves import zip' statement in the top-level namespace.
+
+We avoid the transformation if the zip() call is directly contained in
+iter(<>), list(<>), tuple(<>), sorted(<>), ...join(<>), or for V in <>:.
+Extended by CONTACT Software GmbH
+"""
+
+# Local imports
+from lib2to3 import fixer_base
+from lib2to3.fixer_util import Name, Call, in_special_context
+
+from lib2to3.fixer_util import touch_import
+
+
+class FixZip(fixer_base.ConditionalFix):
+
+    BM_compatible = True
+    PATTERN = """
+    power< name='zip' args=trailer< '(' [any] ')' >
+    >
+    """
+
+    skip_on = "six.moves.zip"
+
+    def transform(self, node, results):
+        if self.should_skip(node):
+            return
+
+        if in_special_context(node):
+            return None
+
+        touch_import(None, u'six.moves', node)
+        name = results['name']
+        name.replace(Name(u'six.moves.zip', name.prefix))
+        new = node.clone()
+        new.prefix = u""
+        new = Call(Name(u"list"), [new])
+        new.prefix = node.prefix
+        return new
---
+++ tests/test_fixers/test_zip.py
@@ -0,0 +1,70 @@
+from fixertestcase import FixerTestCase
+
+
+class Test_zip(FixerTestCase):
+    fixer = "zip"
+
+    def check(self, b, a):
+        self.unchanged("from six.moves import zip; " + b, a)
+        super(Test_zip, self).check(b, a)
+
+    def test_zip_basic(self):
+        b = """x = zip(a, b, c)"""
+        a = """import six.moves\nx = list(six.moves.zip(a, b, c))"""
+        self.check(b, a)
+
+        b = """x = len(zip(a, b))"""
+        a = """import six.moves\nx = len(list(six.moves.zip(a, b)))"""
+        self.check(b, a)
+
+    def test_zip_nochange(self):
+        a = """b.join(zip(a, b))"""
+        self.unchanged(a)
+        a = """(a + foo(5)).join(zip(a, b))"""
+        self.unchanged(a)
+        a = """iter(zip(a, b))"""
+        self.unchanged(a)
+        a = """list(zip(a, b))"""
+        self.unchanged(a)
+        a = """list(zip(a, b))[0]"""
+        self.unchanged(a)
+        a = """set(zip(a, b))"""
+        self.unchanged(a)
+        a = """set(zip(a, b)).pop()"""
+        self.unchanged(a)
+        a = """tuple(zip(a, b))"""
+        self.unchanged(a)
+        a = """any(zip(a, b))"""
+        self.unchanged(a)
+        a = """all(zip(a, b))"""
+        self.unchanged(a)
+        a = """sum(zip(a, b))"""
+        self.unchanged(a)
+        a = """sorted(zip(a, b))"""
+        self.unchanged(a)
+        a = """sorted(zip(a, b), key=blah)"""
+        self.unchanged(a)
+        a = """sorted(zip(a, b), key=blah)[0]"""
+        self.unchanged(a)
+        a = """enumerate(zip(a, b))"""
+        self.unchanged(a)
+        a = """enumerate(zip(a, b), start=1)"""
+        self.unchanged(a)
+        a = """for i in zip(a, b): pass"""
+        self.unchanged(a)
+        a = """[x for x in zip(a, b)]"""
+        self.unchanged(a)
+        a = """(x for x in zip(a, b))"""
+        self.unchanged(a)
+
+    def test_six_import(self):
+        a = "from six.moves import spam, zip, eggs; zip(a, b)"
+        self.unchanged(a)
+
+        b = """from six.moves import spam, eggs\nx = zip(a, b)"""
+        a = """from six.moves import spam, eggs\nimport six.moves\n""" \
+            """x = list(six.moves.zip(a, b))"""
+        self.check(b, a)
+
+        a = "from six.moves import *; zip(a, b)"
+        self.unchanged(a)
